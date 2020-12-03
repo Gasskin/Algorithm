8c55f4ea6e4dc79d4087320f0d6ba92408917e43
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-03 22:58:30 +0800

    MyList 实现中...

--- 数据结构与算法/MyList.h
+++ 数据结构与算法/MyList.h
@@ -35,11 +35,37 @@ struct __list_iterator
 
 	link_type								node; //节点指针
 
-	bool operator==(const self& x) const { return node == x.node; }//比较两个迭代器是否相等，只要比较所指节点是否一样
-	bool operator!=(const self& x) const { return node != x.node; }
+	bool		 operator==(const self& x) const { return node == x.node; }//比较两个迭代器是否相等，只要比较所指节点是否一样
+	bool		 operator!=(const self& x) const { return node != x.node; }
 
-	reference operator*() const { return (*node).data; }//ref返回的是T&
-	pointer operator->() const { return &(operator*()); }
+	reference	 operator*() const { return (*node).data; }//ref返回的是T&，类似int&，直接对应于data数据
+	//不是很能理解为什么要重载->，因为使用迭代器时一般也只是用*iter，而且这里返回的似乎就是&((*node).data)，即&data，不明白有何作用
+	pointer		 operator->() const { return &(operator*()); }
+
+	//self是一个迭代器，而我们最终返回的就是本身，但我们令node=node->next，作为一次++操作
+	self& operator++()//前缀，相当于++i
+	{
+		node = (link_type)((*node).next);
+		return *this;
+	}
+	//还需要注意的是，前缀++我们返回的就是迭代器本身，后缀++返回的是当前迭代器++前的拷贝，然后当前迭代器也会++
+	self operaotr++(int)//后缀，相当于i++(0)
+	{
+		self temp = *this;
+		++* this;
+		return tmp;
+	}
+	self& operator--()
+	{
+		node = (link_type)((*node).prev);
+		return *this;
+	}
+	self operaotr--(int)
+	{
+		self temp = *this;
+		--* this;
+		return tmp;
+	}
 };
 
 //==============================
